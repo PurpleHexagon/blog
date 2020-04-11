6dc47987213968af2e6e6d4e27c49abd96a9ed7c
Jon Day <jon@plantigrate.com>
2020-04-11 15:18:09 +0100

    Initial setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,76 +1,38 @@
 module.exports = {
   siteMetadata: {
-    title: `Sky Lite`,
+    title: `Purple Hexagon`,
     siteUrl: `https://yoursite.com`,
-    description: `A lightweight GatsbyJS starter base with Material-UI and MDX Markdown support.`,
+    description: "Jon Day's technical blog exploring Software Dev, Data Science and more",
     components: {
       appbar: {
         position: "sticky",
         links: [
           {
-            title: "Doc 1",
-            url: "/doc1"
+            title: "Tags",
+            url: "/tags"
           },
           {
-            title: "Doc 2",
-            url: "/doc2"
+            title: "CV",
+            url: "/cv"
           }
         ]
       },
       footer: {
-        copyright: "yoursite.com",
+        copyright: "purplehexagon.com",
         columns: [
           {
-            heading: "Column 1",
+            heading: "Find me on",
             links: [
               {
-                title: "Link 1",
-                url: "#"
+                title: "Github",
+                url: "https://github.com/PurpleHexagon"
               },
               {
-                title: "Link 2",
-                url: "#"
+                title: "Stackoverflow",
+                url: "https://stackoverflow.com/users/1185180/purple-hexagon"
               },
-              {
-                title: "Link 3",
-                url: "#"
-              }
             ]
           },
-          {
-            heading: "Column 2",
-            links: [
-              {
-                title: "Link A",
-                url: "#"
-              },
-              {
-                title: "Link B",
-                url: "#"
-              },
-              {
-                title: "Link C",
-                url: "#"
-              }
-            ]
-          },
-          {
-            heading: "Column 3",
-            links: [
-              {
-                title: "Link x",
-                url: "#"
-              },
-              {
-                title: "Link y",
-                url: "#"
-              },
-              {
-                title: "Link z",
-                url: "#"
-              }
-            ]
-          }
         ]
       }
     },
