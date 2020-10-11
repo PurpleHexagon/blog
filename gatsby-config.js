a457e21dfead3c06c335225b48f9ee811c135bab
Jon Day <jon@plantigrate.com>
2020-10-11 13:55:30 +0100

    Add slash

--- gatsby-config.js
+++ gatsby-config.js
@@ -97,7 +97,7 @@ module.exports = {
         // https://github.com/gatsbyjs/gatsby/issues/15486
         plugins: [
           `gatsby-remark-images`,
-          'gatsby-plugin-remove-trailing-slashes',
+          'gatsby-plugin-force-trailing-slashes',
         ],
         gatsbyRemarkPlugins: [
           {
