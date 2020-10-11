66bc7c1e8ccecf12723f57c5e96bd223eecb00c7
Jon Day <jon@plantigrate.com>
2020-10-11 13:31:53 +0100

    Fix code component

--- src/components/code.js
+++ src/components/code.js
@@ -47,8 +47,7 @@ export const Code = ({ codeString, language, ...props }) => {
             <Highlight {...defaultProps} Prism={Prism} code={codeString} language={language} theme={theme}>
                 {({ className, style, tokens, getLineProps, getTokenProps }) => (
                     <pre className={className} style={style}>
-            {tokens.map((lineimport Prism from "prismjs"
-, i) => (
+            {tokens.map((line, i) => (
                 <div {...getLineProps({ line, key: i })}>
                     {line.map((token, key) => (
                         <span {...getTokenProps({ token, key })} />
